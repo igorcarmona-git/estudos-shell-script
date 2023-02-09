c7e52a9cd105ed16f9634658ebed300fcffba236
Igor Carmona <113251058+igorcarmona-git@users.noreply.github.com>
2023-02-09 16:33:12 -0300

    Programa simples de argumentos

---
+++ argumentos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Um simples programa de argumentos
+
+echo "Arguemento $1"
+echo "Argumento $2"
+
+#Numero de argumentos passados
+
+echo "Foram passados um total de $# argumentos"
+
+#list de todos os argumentos
+
+echo "A Lista de argumentos a seguir: $@"
